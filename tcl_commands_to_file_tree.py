f3bb9c0c7b2b757131140901088409d8a827ba69
Kareem Farid <kareefardi@users.noreply.github.com>
2024-03-07 20:39:56 +0200

    Fix script not using openroad_directory Fix script not running Consistent actions naming

--- tcl_commands_to_file_tree.py
+++ tcl_commands_to_file_tree.py
@@ -11,19 +11,23 @@ now = datetime.datetime.now()
 now = now.strftime("%Y-%m-%d")
 
 
-def get_tcl_files(out_dir):
-    output_file = os.path.join(out_dir, "tcl_interface_files.txt")
+def get_tcl_files(openroad_directory, output_dir):
+    output_file = os.path.join(output_dir, "tcl_interface_files.txt")
     output_stream = open(output_file, "w")
     print(f"{os.path.basename(__file__)}: Generating {output_file}")
-    for root, subdir, files in os.walk("./src"):
+    src_directory = os.path.join(openroad_directory, "src")
+    print(f"{os.path.basename(__file__)}: Using {src_directory}")
+    for root, subdir, files in os.walk(src_directory):
         for file in files:
-            if re.match(r"\./src/[A-z]+/src/[A-z]+\.tcl", os.path.join(root, file)):
+            if re.match(
+                rf"{src_directory}/[A-z]+/src/[A-z]+\.tcl", os.path.join(root, file)
+            ):
                 print(os.path.join(root, file), file=output_stream)
     return output_file
 
 
 def generate_commands(files_list_file, output_dir):
-    tcl_script = "./commands_to_tree.tcl"
+    tcl_script = "./commands_to_file_tree.tcl"
     cmd = ["tclsh", tcl_script, files_list_file, output_dir]
     output = subprocess.run(cmd, stdout=subprocess.PIPE, encoding="utf-8")
     for line in output.stdout.split("\n"):
@@ -32,9 +36,10 @@ def generate_commands(files_list_file, output_dir):
 
 def get_output_dir(openroad_directory):
     output_dir = f"{now}_"
+    command = f"git -C {openroad_directory} rev-parse --short HEAD"
     output_dir += (
         subprocess.run(
-            f"git rev-parse --short HEAD -C {openroad_directory}".split(),
+            command.split(),
             encoding="utf-8",
             stdout=subprocess.PIPE,
         )
@@ -49,9 +54,9 @@ def get_output_dir(openroad_directory):
 def main(openroad_directory):
     output_dir = get_output_dir(openroad_directory)
     Path(output_dir).mkdir(parents=True, exist_ok=True)
-    tcl_files_list = get_tcl_files(output_dir)
+    tcl_files_list = get_tcl_files(openroad_directory, output_dir=output_dir)
     generate_commands(files_list_file=tcl_files_list, output_dir=output_dir)
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
